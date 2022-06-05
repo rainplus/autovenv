3f2c5641a3261aac3615aba2e6666cf74b83d4cd
rainplus <rainplus@foxmail.com>
2022-06-05 22:04:19 +0800

    docs#init

---
+++ README.md
@@ -0,0 +1,20 @@
+## What is it?
+
+If a directory create by "python -m venv " , it will automatically be activate
+venv when you `cd` into it. it will automatically be deactivate when you leave 
+it.
+
+This is great for...
+
+- auto-activating virtualenvs
+- auto-deactivating virtualenvs
+
+How awesome is that!?
+
+### installation
+
+```sh
+git clone git@github.com:rainplus/autovenv-plugin-zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autovenv
+
+```
+
---
+++ autovenv/autovenv.plugin.zsh
@@ -0,0 +1,40 @@
+# Initialization: activate autovenv or report its absence
+() {
+  # Complain if "python -m venv" is not installed
+	if python -c "import venv" >/dev/null 2>&1;then
+	else
+    cat <<END >&2
+-------- AUTOVENV ---------
+Could not find  python3 moudle venv
+Please check if python3 -m venv is correctly installed.
+In the meantime the autovenv plugin is DISABLED.
+--------------------------
+END
+  fi 
+}
+
+[[ $? != 0 ]] && return $?
+
+# The use_venv call below is a reusable command to execute  "python -m venvi $1"
+
+use_env() {
+  local venv
+  venv="$1"
+  echo -n "Create virtualenv $venv now? (Yn) "
+  read answer
+  if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
+    python -m venv "$venv"
+  fi
+}
+
+
+jugde-venv() {
+   if [ -f "./bin/activate" -a -z  "$VIRTUAL_ENV" ];then
+					 source ./bin/activate
+	 elif  [ !  -f "./bin/activate"  -a -n "$VIRTUAL_ENV" ];then
+		 deactivate
+	 fi
+}
+
+
+add-zsh-hook chpwd jugde-venv
